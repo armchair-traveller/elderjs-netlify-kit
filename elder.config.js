32584213c7ca8533bb7876873564dfd755e4d5c0
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-10-20 20:57:17 -0700

    remove a bunch of blog related things

--- elder.config.js
+++ elder.config.js
@@ -18,9 +18,9 @@ module.exports = {
     // disable: ['elderWriteHtmlFileToPublic'], // this is used to disable internal hooks. Uncommenting this would disabled writing your files on build.
   },
   plugins: {
-    '@elderjs/plugin-markdown': {
-      routes: ['blog'],
-    },
+    // '@elderjs/plugin-markdown': { // Markdown plugin, enable if used
+    //   routes: ['blog'],
+    // },
     '@elderjs/plugin-browser-reload': {
       // this reloads your browser when nodemon restarts your server.
       port: 8080,
